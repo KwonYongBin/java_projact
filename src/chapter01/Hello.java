2c4249a52da2d339d552a8bd3cf11bced6c74e27
KwonYongBin <dydqls6789@naver.com>
2025-06-23 16:46:15 +0900

    initial commit

--- src/chapter01/Hello.java
+++ src/chapter01/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("welcom to JAVA World");
-
+		System.out.println("환영합니다 자바 월드"); //한상 마무리는 (;)을 사용하여 마무리한다.
 	}
 
 }
